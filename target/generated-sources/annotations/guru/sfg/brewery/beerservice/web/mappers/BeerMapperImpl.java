68f7f90f143e6019bb8e8cb7f79534ee40eef43a
Luke <kemnerluke@gmail.com>
2020-01-23 12:45:48 -0500

    changed server.port

--- target/generated-sources/annotations/guru/sfg/brewery/beerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/brewery/beerservice/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-01-23T12:05:58-0500",
+    date = "2020-01-23T12:10:01-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (JetBrains s.r.o)"
 )
 @Component
