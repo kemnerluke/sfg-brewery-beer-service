f5ff752e3a7e82af89ce99d6db597239d4e2c059
Luke <kemnerluke@gmail.com>
2020-02-02 17:18:40 -0500

    dded inventory check for beerorderlistner

--- src/main/java/guru/sfg/brewery/beerservice/services/order/BeerOrderListener.java
+++ src/main/java/guru/sfg/brewery/beerservice/services/order/BeerOrderListener.java
@@ -6,6 +6,7 @@ import guru.sfg.brewery.beerservice.config.JmsConfig;
 import guru.sfg.brewery.beerservice.domain.Beer;
 import guru.sfg.brewery.beerservice.model.BeerStyleEnum;
 import guru.sfg.brewery.beerservice.repositories.BeerRepository;
+import guru.sfg.brewery.beerservice.services.inventory.BeerInventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -25,9 +27,11 @@ public class BeerOrderListener {
 
    // private final JmsTemplate jmsTemplate;
     private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
     private final RabbitTemplate rabbitTemplate;
 
     public static final String NEW_BEER_ORDER_QUEUE = "new-beer-order";
+
 
 
 
@@ -37,15 +41,30 @@ public class BeerOrderListener {
     public void listen(){
 
         BeerOrder beerOrder= (BeerOrder) rabbitTemplate.receiveAndConvert(JmsConfig.NEW_BEER_ORDER_QUEUE);
- log.debug("im here in beer order inventorty"+beerOrder.toString());
 
         if (beerOrder != null) {
 
-            beerOrder.getBeerOrderLines().forEach(beerOrderLine -> {
+
+
+                beerOrder.getBeerOrderLines().forEach(beerOrderLine -> {
+
                 log.debug("Beer Order Line" + beerOrderLine.toString());
                 Beer beer = beerRepository.findByUpc(beerOrderLine.getUpc());
-                rabbitTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, beer);
-                log.debug("Sending Beer Order to Brewing Request");
+
+                Integer invQOH = beerInventoryService.getOnhandInventory(beer.getId());
+
+                log.debug("Min Onhand is: " + beer.getMinOnHand());
+                log.debug("Inventory is: "  + invQOH);
+
+                if(beer.getMinOnHand() >= invQOH) {
+                    rabbitTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, beer);
+                    log.debug("Sending Beer Order to Brewing Request");
+
+
+                }
+                else{
+                    log.debug("No Beers need to be Brew");
+                }
             });
         }
         else{
